1116401c3671e62118138fc3ccad93c25a4a418c
Iftekar <neoraf11@yahoo.com>
2013-07-09 16:23:05 -0400

    Guessing Game

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-secret_number = (1 + rand(99))
+number = (1 + rand(99)).to_s
 
 get "/" do 
-	erb :index, :locals => {:number => secret_number}
+	if params["guess"].to_i > number.to_i
+		guess = "Too High!"
+	elsif params["guess"].to_i < number.to_i
+		guess = "Too Low"
+	else
+		guess = "Correct"
+	end
+	erb :index, :locals => {:number => number, :guess => guess}
 end
