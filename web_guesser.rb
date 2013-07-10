3baf9ed1ba4b57ee6d42e4482f46e59dc60234ee
Iftekar Rafi <neoraf11@yahoo.com>
2013-07-10 14:51:44 -0400

    Update web_guesser.rb

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,8 +2,17 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 number = (1 + rand(99)).to_s
-
+chances = 5
+chances_before = 0
 get "/" do
+
+	if chances > 0 && chances_before != params["guess"] 
+		chances = chances - 1
+	elsif chances < 1 
+		chances = 8
+		number = (rand(99)+1).to_s
+	end
+		
 	if params["guess"].to_i > number.to_i + 5
 		guess = "Way Too High"
 	elsif params["guess"].to_i < number.to_i - 5
@@ -15,5 +24,6 @@ get "/" do
 	else
 		guess = "Correct, the secret number was #{number}"
 	end
+
 	erb :index, :locals => {:number => number, :guess => guess}
 end
